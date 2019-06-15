e59229814092a4da1e690f1e1da176c6a85cce00
yaser <yaserbahrami@outlook.com>
2019-06-15 18:41:43 +0430

    start TDD

---
+++ FizzBuzz/Brain.swift
@@ -0,0 +1,16 @@
+//
+//  Brain.swift
+//  FizzBuzz
+//
+//  Created by yaser on 6/15/19.
+//  Copyright © 2019 Bahrami. All rights reserved.
+//
+
+import Foundation
+
+class Brain {
+    
+    func isDivisibleByThree(number: Int) -> Bool{
+        return true
+    }
+}
--- FizzBuzzTests/BrainTests.swift
+++ FizzBuzzTests/BrainTests.swift
@@ -1,5 +1,5 @@
 //
-//  FizzBuzzTests.swift
+//  BrainTests.swift
 //  FizzBuzzTests
 //
 //  Created by yaser on 6/15/19.
@@ -9,7 +9,7 @@
 import XCTest
 @testable import FizzBuzz
 
-class FizzBuzzTests: XCTestCase {
+class BrainTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -18,17 +18,10 @@ class FizzBuzzTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    
+    func testIsDivisibleByThree(){
+        let brain = Brain()
+        let result = brain.isDivisibleByThree(number: 3)
+        XCTAssertEqual(result, true)
     }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
