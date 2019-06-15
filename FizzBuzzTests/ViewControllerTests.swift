18c1e129b6afe71faf98ddc57031576a2901a23f
yaser <yaserbahrami@outlook.com>
2019-06-16 00:18:33 +0430

    make main view and initilize unit test for viewcontroller

---
+++ FizzBuzzTests/ViewControllerTests.swift
@@ -0,0 +1,32 @@
+//
+//  ViewControllerTests.swift
+//  FizzBuzzTests
+//
+//  Created by yaser on 6/16/19.
+//  Copyright © 2019 Bahrami. All rights reserved.
+//
+
+import XCTest
+@testable import FizzBuzz
+
+class ViewControllerTests: XCTestCase {
+    var viewController : ViewController!
+
+    override func setUp() {
+        super.setUp()
+        
+        let storyBoard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        viewController = storyBoard.instantiateViewController(withIdentifier: "ViewController") as? ViewController
+        UIApplication.shared.keyWindow?.rootViewController = viewController
+        
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+    
+    func test() {
+        XCTAssertTrue(true)
+    }
+
+}
