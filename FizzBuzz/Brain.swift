759fca6359fae6b7c15757b028051f54515ad253
yaser <yaserbahrami@outlook.com>
2019-06-15 19:48:22 +0430

    refactor brain

--- FizzBuzz/Brain.swift
+++ FizzBuzz/Brain.swift
@@ -11,10 +11,6 @@ import Foundation
 class Brain {
     
     func isDivisibleBy(divisor: Int, number: Int) -> Bool {
-        if number % divisor == 0{
-            return true
-        }else{
-            return false
-        }
+        return number % divisor == 0
     }
 }
